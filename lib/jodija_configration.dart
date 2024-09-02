1b750426635c0e3f0c0ea3b902c88508a4fc2db8
HP <moaz.salaheddin>
2024-09-02 15:35:12 +0300

    congigration

---
+++ lib/jodija_configration.dart
@@ -0,0 +1,17 @@
+import 'package:JoDija_DataSource/source/http/crud_http_sources.dart';
+import 'package:firebase_core/firebase_core.dart';
+abstract class AppConfigration {
+  static AppType appType = AppType.App;
+  static EnvType envType = EnvType.dev;
+  static BackendState backendState = BackendState.remote;
+
+    Future FirebaseInit() ;
+  Future backenRoutsdInit() ;
+
+
+}
+enum AppType { DashBord, App }
+
+enum EnvType { localDev, dev, prod }
+
+enum BackendState { local, remote }
