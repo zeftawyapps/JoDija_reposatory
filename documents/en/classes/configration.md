83bf337d7e4cd9dd51548a11339f238deefbc47e
HP <moaz.salaheddin>
2025-01-14 19:48:36 +0200

    docmument 2

--- documents/en/classes/configration.md
+++ documents/en/classes/configration.md
@@ -1,8 +1,48 @@
-### configurations classes 
+## Configurations Classes
 
-after you create the project you need to confrontation the project to 
-defined the base url and the firebase configuration that you will use in the project
+After you create the project, you need to configure the project to
+define the base URL and the Firebase configuration that you will use in the project.
 
-```dart 
+## DataSourceConfiguration Class
 
-```
+This class provides functions to help you determine if you should communicate with Firebase or a regular server.
+
+**Methods**
+
+* **FirebaseInit(String path)**
+    * Initializes Firebase with configuration from a JSON file.
+    * Reads Firebase configuration from the specified JSON file.
+    * Selects the appropriate configuration based on the `envType` (production or development).
+    * Initializes Firebase with the selected configuration.
+    * Throws an exception if initialization fails.
+
+* **FirebaseInitFromDataJson(Map<String, dynamic> path)**
+    * Initializes Firebase with configuration from a provided data map.
+    * Selects the appropriate configuration based on the `envType` (production or development).
+    * Initializes Firebase with the selected configuration.
+    * Throws an exception if initialization fails.
+
+* **backendRoutedInit(String path)**
+    * Initializes backend routing with the base URL from a JSON file.
+    * Reads base URLs from the specified JSON file.
+    * Selects the base URL based on the `backendState` (local, remote_dev, or remote_prod).
+    * Sets the selected base URL for backend communication.
+    * Throws an exception if initialization fails.
+
+* **backendRoutedInitFromJson(Map<String, dynamic> data)**
+    * Initializes backend routing with the base URL from a provided data map.
+    * Selects the base URL based on the `backendState` (local, remote_dev, or remote_prod).
+    * Sets the selected base URL for backend communication.
+    * Throws an exception if initialization fails.
+
+### Properties
+
+* **appType:** (getter/setter) Gets or sets the application type.
+* **backendState:** (getter/setter) Gets or sets the backend state.
+* **envType:** (getter/setter) Gets or sets the environment type.
+
+**Note:**
+
+* Private fields (`_appType`, `_backendState`, `_envType`) are used to encapsulate the internal state of the class.
+* Proper exception handling is crucial for robust initialization.
+* Consider using a dependency injection framework to manage dependencies and improve testability.
--- lib/jodija_configration.dart
+++ lib/jodija_configration.dart
@@ -1,11 +1,27 @@
-import 'package:JoDija_DataSource/source/http/crud_http_sources.dart';
 import 'package:JoDija_DataSource/utilis/json_reader/json_asset_reader.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'https/http_urls.dart';
 
+
+
+
+/// Abstract class for configuring data sources, including Firebase and backend routing.
+///
+/// This class provides methods to initialize Firebase and backend routing configurations
+/// from JSON files or data maps. It also includes properties to manage the application type,
+/// backend state, and environment type.
 abstract class DataSourceConfigration {
+
+
+  /// Initializes Firebase with the configuration specified in the JSON file at the given path.
+  ///
+  /// This function reads the Firebase configuration from a JSON file and initializes Firebase
+  /// based on the environment type (production or development).
+  ///
+  /// \[param\] path The path to the JSON file containing the Firebase configuration.
+  /// \[throws\] Exception if an error occurs during Firebase initialization.
   Future FirebaseInit(String path) async {
     try {
       WidgetsFlutterBinding.ensureInitialized();
@@ -36,6 +52,52 @@ abstract class DataSourceConfigration {
     }
   }
 
+  /// Initializes Firebase with the configuration provided in the given data map.
+  ///
+  /// This function initializes Firebase based on the environment type (production or development)
+  /// using the configuration provided in the data map.
+  ///
+  /// \[param\] path The map containing the Firebase configuration.
+  /// \[throws\] Exception if an error occurs during Firebase initialization.
+   Future FirebaseInitFromDataJson(Map<String , dynamic> path) async {
+    try {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      var data =  path; 
+      var firebaseConfig = data['firebaseConfig'];
+      if (this.envType == EnvType.prod) {
+        var prod = firebaseConfig['prod'];
+        await Firebase.initializeApp(
+            options: FirebaseOptions(
+                apiKey: prod['apiKey'],
+                appId: prod['appId'],
+                messagingSenderId: prod['messagingSenderId'],
+                projectId: prod['projectId'],
+                storageBucket: prod['storageBucket']));
+      } else {
+        var dev = firebaseConfig['dev'];
+        await Firebase.initializeApp(
+            options: FirebaseOptions(
+                apiKey: dev['apiKey'],
+                appId: dev['appId'],
+                messagingSenderId: dev['messagingSenderId'],
+                projectId: dev['projectId'],
+                storageBucket: dev['storageBucket']));
+      }
+    } on Exception catch (e) {
+      print(e);
+    }
+  }
+
+
+
+  /// Initializes the backend routing with the base URL specified in the JSON file at the given path.
+  ///
+  /// This function reads the base URLs from a JSON file and sets the base URL
+  /// based on the backend state (local, remote\_dev, or remote\_prod).
+  ///
+  /// \[param\] path The path to the JSON file containing the base URLs.
+  /// \[throws\] Exception if an error occurs during the initialization.
   Future backendRoutedInit(String path) async {
     WidgetsFlutterBinding.ensureInitialized();
 
@@ -53,11 +115,51 @@ abstract class DataSourceConfigration {
     HttpUrlsEnveiroment(baseUrl: BaseUrl);
   }
 
-  AppType appType = AppType.App;
+  /// Initializes the backend routing with the base URL provided in the given data map.
+  ///
+  /// This function reads the base URLs from the provided data map and sets the base URL
+  /// based on the backend state (local, remote\_dev, or remote\_prod).
+  ///
+  /// \[param\] data The map containing the base URLs.
+  /// \[throws\] Exception if an error occurs during the initialization.
+  Future backendRoutedInitFromJson(Map<String, dynamic> data) async {
+    WidgetsFlutterBinding.ensureInitialized();
 
-  BackendState backendState = BackendState.remote_dev ;
+    var baseUrls = data['baseUrls'];
+    String BaseUrl;
 
-  EnvType envType = EnvType.dev;
+    if (backendState == BackendState.local) {
+      BaseUrl = baseUrls['local'];
+    } else if (backendState == BackendState.remote_dev) {
+      BaseUrl = baseUrls['remote_dev'];
+    } else {
+      BaseUrl = baseUrls['remote_prod'];
+    }
+    HttpUrlsEnveiroment(baseUrl: BaseUrl);
+  }
+
+  AppType _appType = AppType.App;
+
+  BackendState _backendState = BackendState.remote_dev;
+
+  EnvType _envType = EnvType.dev;
+
+  AppType get appType => _appType;
+  set appType(AppType value) {
+    _appType = value;
+  }
+
+  BackendState get backendState => _backendState;
+  set backendState(BackendState value) {
+    _backendState = value;
+  }
+
+  EnvType get envType => _envType;
+  set envType(EnvType value) {
+    _envType = value;
+  }
+
+
 }
 
 enum AppType { DashBord, App }
